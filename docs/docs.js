f807092aca9c371038dbc9f851fb68fdaea5c397
Matthew Capewell <matthew.capewell@ymx.yokogawa.com>
2017-02-13 16:30:46 +0000

    Added simple menu styling and changed docs to use it

--- docs/docs.js
+++ docs/docs.js
@@ -1,13 +1,26 @@
 Vue.component('docs-menu', {
-    template: '<ul>\
-        <li><a href="index.html">Home</a></li>\
-        <li><a href="typography.html">Typography</a></li>\
-        <li><a href="grid.html">Grid</a></li>\
-        <li><a href="button.html">Button</a></li>\
-        <li><a href="button-group.html">Button Group</a></li>\
-        <li><a href="table.html">Table</a></li>\
-        <li><a href="form.html">Form</a></li>\
-    </ul>'
+    props: ['currentPage'],
+
+    template: '<ul class="menu">\
+        <li v-for="item in menuItems" v-bind:class="item.url === currentPage ? \'active\' : \'\'">\
+            <a v-bind:href="item.url">{{ item.title }}</a>\
+        </li>\
+    </ul>',
+    
+    data: function () {
+        return {
+            menuItems: [
+                { url: "index.html", title: "Home" },
+                { url: "typography.html", title: "Typography" },
+                { url: "grid.html", title: "Grid" },
+                { url: "button.html", title: "Button" },
+                { url: "button-group.html", title: "Button Group" },
+                { url: "table.html", title: "Table" },
+                { url: "form.html", title: "Form" },
+                { url: "menu.html", title: "Menu" }
+            ]
+        }
+    }
 });
 
 var app = new Vue({
